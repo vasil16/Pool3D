78c66c9b76becd3c831d2cb3746ee3e6c8a1810d
arvasil <133312794+arvasil@users.noreply.github.com>
2025-06-24 03:21:48 +0530

    stick issue fix

--- Assets/StickTrigger.cs
+++ Assets/StickTrigger.cs
@@ -1,83 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class StickTrigger : MonoBehaviour
 {
-    List<MeshRenderer> objectMeshes;
-
-    bool overLap;
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        //if(HasColliderBelow())
-        //{
-        //    //StartCoroutine(RotateUp(true));
-        //}
-        //else
-        //{
-        //    //transform.parent.localRotation = Quaternion.Euler(0, 0, -5.927f);
-        //}
-        //StartCoroutine(HasColliderBelow());
-        //if (GetComponent<MeshRenderer>().bounds.Contains(objectMeshes.))
-    }
+    bool up;
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("enter");
-        overLap = true;
-        
+        up = true;
+        RotateUp(-10);
     }
 
     public void OnTriggerExit(Collider other)
     {
-        overLap = false;
-        float rotZ = transform.parent.eulerAngles.z;
+        if (!up) return;
+        RotateUp(10);
     }
 
-    public bool noOverLap()
+    void RotateUp(float value)
     {
-        return overLap;
-    }
-
-    IEnumerator RotateUp(bool up)
-    {
-        if(up)
-        {
-            while(overLap)
-            {
-                transform.parent.Rotate(Vector3.forward * -1);
-                yield return null;
-            }
-        }
-        else
-        {
-            transform.parent.Rotate(Vector3.forward);
-            yield return null;            
-        }
-    }
-
-    IEnumerator HasColliderBelow()
-    {        
-        if(GetComponent<Rigidbody>().SweepTest(Vector3.down,out RaycastHit hit))
-        {
-            //if (hit.transform.gameObject.tag is "playBall" or "cushion")
-            //    Debug.Log("below");
-            //    return true;
-            while(hit.distance >0 && hit.distance < 0.02f)
-            {
-                transform.parent.Rotate(Vector3.forward);
-                yield return null;
-            }
-        }
-        else
-        {
-            transform.parent.localRotation = Quaternion.Euler(0, 0, -5.927f);
-        }
+        transform.DORotateQuaternion(Quaternion.Euler (transform.rotation.x + value, transform.rotation.y, transform.rotation.z), 0.4f);
+        up = false;
     }
 }
